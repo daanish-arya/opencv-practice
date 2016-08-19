9a4a314458f5b24f8045eedfefca6e36f989ada4
Daanish Arya <daanish.arya@daanishs-mbp.au.ibm.com>
2016-08-19 15:30:13 +1000

    otis

--- subtraction.py
+++ subtraction.py
@@ -6,7 +6,6 @@ imgL = cv2.imread('0A0B4D50-A986-4659-9E5E-2A9BBF19D594_OD_1_L.jpg', cv2.CV_LOAD
 imgR = cv2.imread('0A0B4D50-A986-4659-9E5E-2A9BBF19D594_OD_1_R.jpg', cv2.CV_LOAD_IMAGE_GRAYSCALE)
 
 diff = cv2.add(imgL, imgR)
-# diff = cv2.subtract(orig, diff)
 # ret, thresh = cv2.threshold(imgray, 100, 255, 0)
 # thresh = cv2.adaptiveThreshold(diff, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 433, 42)
 # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -19,20 +18,22 @@ cols = disparity.shape[1]
 # plt.figure()
 # plt.imshow(imgL, 'gray')
 # '''
-print disparity[200]
-zero_index = (disparity == 0)
-print zero_index.shape
-indices_zero = np.arange(rows)[zero_index-1]
+# print disparity[200]
+ayus = [[0, 0]]
+for i in range(0, rows):
+    zero_index = np.where(disparity[i] == 0)
+    for item in zero_index[0]:
+        ayus.append([i, item+1])
 
-
-'''
-otis = np.array([0, 0])
-for i in range(1, rows+1):
-
-    print pixel_num
-print otis
+for i in range(1, rows-200):
+    for j in range(200, cols-200):
+        ret, thresh = cv2.threshold(diff[(i-200):(i+200), (j-200):(j+200)], 90, 255, 0)
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(imgL, contours, -1, (255, 0, 0), 1)
 # '''
-'''
+
+# '''
+# '''
 plt.figure()
 #plt.title('Disparity map')
 #plt.imshow(disparity)
